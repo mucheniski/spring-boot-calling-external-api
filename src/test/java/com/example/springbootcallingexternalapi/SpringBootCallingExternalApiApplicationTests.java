4609d2a6a33497973d167ad9f9fd3a09de103488
Diego Mucheniski <mucheniski@gmail.com>
2022-08-24 17:26:41 -0300

    projeto criado

---
+++ src/test/java/com/example/springbootcallingexternalapi/SpringBootCallingExternalApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootcallingexternalapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCallingExternalApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
