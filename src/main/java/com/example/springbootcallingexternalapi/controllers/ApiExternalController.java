c1d777e28030b7399c9e3d5a20bd3ca27fdfa4a4
Diego Mucheniski <mucheniski@gmail.com>
2022-08-24 17:34:31 -0300

    chamando url interna com RestTemplate

--- src/main/java/com/example/springbootcallingexternalapi/controllers/ApiExternalController.java
+++ src/main/java/com/example/springbootcallingexternalapi/controllers/ApiExternalController.java
@@ -3,14 +3,23 @@ package com.example.springbootcallingexternalapi.controllers;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
-@RequestMapping("/hello")
+@RequestMapping("/tests")
 public class ApiExternalController {
 
-    @GetMapping
+    @GetMapping("/hello")
     public String hello() {
         return "Hello";
+    }
+
+    @GetMapping("/callhello")
+    private String getHelloClient() {
+        String url = "http://localhost:8080/tests/hello";
+        RestTemplate restTemplate = new RestTemplate();
+        String result = restTemplate.getForObject(url, String.class);
+        return result;
     }
 
 }
