0a52d66b5d40611071b9c35d71e00eedd1fe4c14
Diego Mucheniski <mucheniski@gmail.com>
2022-08-24 19:16:01 -0300

    controller chamando apis

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # spring-boot-calling-external-api
 https://acervolima.com/como-chamar-ou-consumir-api-externa-no-spring-boot/
+
+# Rick And Morty Api
+https://rickandmortyapi.com/api/character  
--- src/main/java/com/example/springbootcallingexternalapi/controllers/ApiExternalController.java
+++ src/main/java/com/example/springbootcallingexternalapi/controllers/ApiExternalController.java
@@ -5,6 +5,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.Arrays;
+import java.util.List;
+
 @RestController
 @RequestMapping("/tests")
 public class ApiExternalController {
@@ -15,11 +18,19 @@ public class ApiExternalController {
     }
 
     @GetMapping("/callhello")
-    private String getHelloClient() {
+    public String getHelloClient() {
         String url = "http://localhost:8080/tests/hello";
         RestTemplate restTemplate = new RestTemplate();
         String result = restTemplate.getForObject(url, String.class);
         return result;
+    }
+
+    @GetMapping("rickandmorty")
+    public Object getCharacter() {
+        String url = "https://rickandmortyapi.com/api/character/2";
+        RestTemplate restTemplate = new RestTemplate();
+        Object character = restTemplate.getForObject(url, Object[].class);
+        return character;
     }
 
 }
